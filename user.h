12bb9d12c61babbea6b48dee89d2298e9ae303d6
S.M. Sadat Intishar <92873867+Sadat30@users.noreply.github.com>
2026-08-28 09:42:25 +0600

    Update unlink and atoi function signatures

--- user.h
+++ user.h
@@ -37,7 +37,7 @@ int kill(int);
 int exec(char*, char**);
 int open(const char*, int);
 int mknod(const char*, short, short);
-int unlink(const char*, const char*);
+int unlink(const char*);
 int fstat(int fd, struct stat*);
 int link(const char*, const char*);
 int mkdir(const char*);
